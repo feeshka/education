2825bf41e18d075f62cdd7f69196fbc7f09f481f
Nikita Mikhailov <nikitamikh2015@yandex.ru>
2024-01-15 11:17:19 +0300

    Добавил проект для бенчмарков. Добавил бенчмарк для ToList

---
+++ benchmarks/Benchmarks/Benchmarks/ToList/ToListBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Immutable;
+
+namespace Benchmarks.ToList;
+
+public class ToListBenchmarks
+{
+    private const int _count = 1_000_000;
+
+    IEnumerable<int> Do(Func<IEnumerable<int>, IEnumerable<int>> function) =>
+        function(function(Enumerable.Range(0, _count)));
+
+    [Benchmark]
+    public ImmutableArray<int> ToImmutableArray() => (ImmutableArray<int>)Do(x => x.ToImmutableArray());
+
+    [Benchmark]
+    public int[] ToArray() => (int[])Do(x => x.ToArray());
+
+    [Benchmark]
+    public List<int> ToList() => (List<int>)Do(x => x.ToList());
+
+    [Benchmark]
+    public IImmutableList<int> ToImmutableList() => (IImmutableList<int>)Do(x => x.ToImmutableList());
+}
