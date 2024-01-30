5ffdaf7a770d12275195c14f7e727c8a13558b68
Nikita Mikhailov <nikitamikh2015@yandex.ru>
2024-01-30 15:37:24 +0300

    Добавил файлы сценариев для презентации SpecFlow

---
+++ specflow-tests/SpecFlowTests/tests/SpecFlow.ApiTests/Examples/StepDefinition.cs
@@ -0,0 +1,55 @@
+﻿using SpecFlowTests.Common;
+
+namespace SpecFlowTests.Domain.Tests.Features.Examples;
+
+[Binding]
+public class StepDefinition
+{
+    [Given(@"организация (.*)")]
+    public void GivenОрганизация(string organizationName)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"в организацию добавлен пользователь:")]
+    public void GivenВОрганизациюДобавленПользователь(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"пользователь успешно добавлен")]
+    public void ThenПользовательУспешноДобавлен()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"список пользователей организации (.*):")]
+    public void ThenСписокПользователейОрганизации(string organizationName, Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"было отправлено сообщение в telegram:")]
+    public void ThenБылоОтправленоСообщениеВTelegram(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"я добавляю в организацию (.*) нового пользователя:")]
+    public void WhenЯДобавляюВОрганизациюНовогоПользователя(string organizationName, Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"я аутентифицирован от имени пользователя (.*) с паролем (.*)")]
+    public void GivenЯАутентифицированОтИмениПользователяСПаролем(string userPhone, string password)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"я должен получить ошибку с кодом (.*)")]
+    public void ThenЯДолженПолучитьОшибкуСКодом(string errorCode)
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
